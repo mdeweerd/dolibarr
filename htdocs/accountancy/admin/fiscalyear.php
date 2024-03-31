d2ef5259c96386f59c13942f38c9557d6c1d3a28
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 05:01:49 +0200

    Do not use deprecated statut, fk_statut, paye

--- htdocs/accountancy/admin/fiscalyear.php
+++ htdocs/accountancy/admin/fiscalyear.php
@@ -1,6 +1,8 @@
 <?php
+<<<<<<< HEAD
 /* Copyright (C) 2013-2025	Alexandre Spangaro			<alexandre@inovea-conseil.com>
  * Copyright (C) 2024-2025  Frédéric France				<frederic.france@free.fr>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -184,7 +186,6 @@ if ($result) {
 			$fiscalyearstatic->label = $obj->label;
 			$fiscalyearstatic->date_start = $obj->date_start;
 			$fiscalyearstatic->date_end = $obj->date_end;
-			$fiscalyearstatic->statut = $obj->status;	// deprecated
 			$fiscalyearstatic->status = $obj->status;
 
 			print '<tr class="oddeven">';
