af70268dd77122e6b5a80f5de2b9ead4e3ce2e59
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 05:19:31 +0200

    Do not use deprecated statut, fk_statut, paye

--- htdocs/compta/index.php
+++ htdocs/compta/index.php
@@ -363,7 +363,6 @@ if ((isModEnabled('fournisseur') && !getDolGlobalString('MAIN_USE_NEW_SUPPLIERMO
 				$facstatic->total_ttc = $obj->total_ttc;
 				$facstatic->statut = $obj->status;	// deprecated
 				$facstatic->status = $obj->status;
-				$facstatic->paye = $obj->paye;	// deprecated
 				$facstatic->paid = $obj->paye;
 				$facstatic->type = $obj->type;
 				$facstatic->ref_supplier = $obj->ref_supplier;
