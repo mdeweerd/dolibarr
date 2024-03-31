b9c73bfd50abc7ab695006a3590fe9343de96624
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 05:02:02 +0200

    Do not use deprecated statut, fk_statut, paye

--- htdocs/compta/prelevement/class/bonprelevement.class.php
+++ htdocs/compta/prelevement/class/bonprelevement.class.php
@@ -228,12 +228,6 @@ class BonPrelevement extends CommonObject
 	 * @var float
 	 */
 	public $amount;
-
-	/**
-	 * @var int	Status
-	 * @deprecated Use $status
-	 */
-	public $statut;
 	/**
 	 * @var int|null	Status
 	 */
@@ -527,7 +521,6 @@ class BonPrelevement extends CommonObject
 				if (empty($this->status)) {		// Value is sometimes null in database
 					$this->status = 0;
 				}
-				$this->statut         = $this->status; // For backward compatibility
 
 				$this->fetched = 1;
 
@@ -756,7 +749,6 @@ class BonPrelevement extends CommonObject
 			// End of procedure
 			if ($error == 0) {
 				$this->date_credit = $date;		// date credit or debit
-				$this->statut = self::STATUS_CREDITED;
 				$this->status = self::STATUS_CREDITED;
 
 				$this->db->commit();
@@ -812,7 +804,6 @@ class BonPrelevement extends CommonObject
 
 			if ($error == 0) {
 				$this->date_trans = $date;
-				$this->statut = self::STATUS_TRANSFERED;
 				$this->status = self::STATUS_TRANSFERED;
 				$this->user_trans = $user->id;
 
@@ -2830,7 +2821,7 @@ class BonPrelevement extends CommonObject
 	 */
 	public function getLibStatut($mode = 0)
 	{
-		return $this->LibStatut((isset($this->status) ? $this->status : $this->statut), $mode);
+		return $this->LibStatut($this->status, $mode);
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
