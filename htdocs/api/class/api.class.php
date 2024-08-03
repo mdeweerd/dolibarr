e2799a30fa8ba2d24d9ad821aff5edb50309a272
MDW <mdeweerd@users.noreply.github.com>
2024-08-03 17:16:26 +0200

    fixup! Make _cleanObjectDatas covariant, phpdoc _validate

--- htdocs/api/class/api.class.php
+++ htdocs/api/class/api.class.php
@@ -1,6 +1,4 @@
 <?php
-/* Copyright (C) 2015   	Jean-François Ferry     <jfefe@aternatik.fr>
- * Copyright (C) 2016		Laurent Destailleur		<eldy@users.sourceforge.net>
 /* Copyright (C) 2015       Jean-François Ferry     <jfefe@aternatik.fr>
  * Copyright (C) 2016	    Laurent Destailleur		<eldy@users.sourceforge.net>
  * Copyright (C) 2020-2025  Frédéric France			<frederic.france@free.fr>
