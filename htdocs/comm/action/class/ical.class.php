3f00468147dfc12cfdc7a7c5a8cf55e620463ade
MDW <mdeweerd@users.noreply.github.com>
2024-10-21 09:47:42 +0200

    Qual: Fix phan notices in class files

--- htdocs/comm/action/class/ical.class.php
+++ htdocs/comm/action/class/ical.class.php
@@ -45,7 +45,7 @@ class ICal
 	public $file_text;
 
 	/**
-	 * @var array Array to save iCalendar parse data
+	 * @var  array<string,array<string,array<string,int>>>|array<string,array<'VEVENT'|'VFREEBUSY'|'VTODO',array<int,array<string,string>>>>|array<string,array<string,int>>|array<string,array<string,string>>	Array to save iCalendar parse data
 	 */
 	public $cal;
 
@@ -138,7 +138,7 @@ class ICal
 	 * @param	string	 	$uri			Url
 	 * @param	string		$usecachefile	Full path of a cache file to use a cache file
 	 * @param	int			$delaycache		Delay in seconds for cache (by default 3600 secondes)
-	 * @return	array|string
+	 * @return  string|array<string,array<string,array<string,int>>>|array<string,array<'VEVENT'|'VFREEBUSY'|'VTODO',array<int,array<string,string>>>>|array<string,array<string,int>>|array<string,array<string,string>>
 	 */
 	public function parse($uri, $usecachefile = '', $delaycache = 3600)
 	{
@@ -277,6 +277,7 @@ class ICal
 			$key = $this->last_key;
 			switch ($type) {
 				case 'VEVENT':
+					// @phan-suppress-current-line PhanTypeMismatchDimFetch
 					$value = $this->cal[$type][$this->event_count][$key].$value;
 					break;
 				case 'VFREEBUSY':
@@ -326,7 +327,7 @@ class ICal
 	 * Parse text "XXXX:value text some with : " and return array($key = "XXXX", $value="value");
 	 *
 	 * @param 	string 	$text	Text
-	 * @return 	array
+	 * @return 	array{0:string,1:string}
 	 */
 	public function retun_key_value($text)
 	{
@@ -351,7 +352,7 @@ class ICal
 	 * Parse RRULE  return array
 	 *
 	 * @param 	string 	$value	string
-	 * @return 	array
+	 * @return 	array<string,string>
 	 */
 	public function ical_rrule($value)
 	{
@@ -425,14 +426,14 @@ class ICal
 	/**
 	 * Return sorted eventlist as array or false if calendar is empty
 	 *
-	 * @return array|false
+	 * @return array<int,array<string,string>>|false
 	 */
 	public function get_sort_event_list()
 	{
 		// phpcs:enable
 		$temp = $this->get_event_list();
 		if (!empty($temp)) {
-			usort($temp, array(&$this, "ical_dtstart_compare"));
+			usort($temp, array($this, "ical_dtstart_compare"));  // false-positive @phpstan-ignore-line
 			return $temp;
 		} else {
 			return false;
@@ -443,9 +444,9 @@ class ICal
 	/**
 	 * Compare two unix timestamp
 	 *
-	 * @param 	array 	$a		Operand a
-	 * @param 	array 	$b		Operand b
-	 * @return 	integer
+	 * @param 	array{DTSTART:array{unixtime:string}} 	$a		Operand a
+	 * @param 	array{DTSTART:array{unixtime:string}}  	$b		Operand b
+	 * @return 	int
 	 */
 	public function ical_dtstart_compare($a, $b)
 	{
@@ -457,55 +458,55 @@ class ICal
 	/**
 	 * Return eventlist array (not sorted eventlist array)
 	 *
-	 * @return array
+	 * @return array<int,array<string,string>>
 	 */
 	public function get_event_list()
 	{
 		// phpcs:enable
-		return (empty($this->cal['VEVENT']) ? array() : $this->cal['VEVENT']);
+		return (empty($this->cal['VEVENT']) ? array() : $this->cal['VEVENT']);  // false-positive @phpstan-ignore-line
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
 	/**
 	 * Return freebusy array (not sort eventlist array)
 	 *
-	 * @return array
+	 * @return array<int,array<string,string>>
 	 */
 	public function get_freebusy_list()
 	{
 		// phpcs:enable
-		return (empty($this->cal['VFREEBUSY']) ? array() : $this->cal['VFREEBUSY']);
+		return (empty($this->cal['VFREEBUSY']) ? array() : $this->cal['VFREEBUSY']);  // false-positive @phpstan-ignore-line
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
 	/**
 	 * Return to do array (not sorted todo array)
 	 *
-	 * @return array
+	 * @return array<int,array<string,string>>
 	 */
 	public function get_todo_list()
 	{
 		// phpcs:enable
-		return $this->cal['VTODO'];
+		return $this->cal['VTODO'];  // false positive @phpstan-ignore-line
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
 	/**
 	 * Return base calendar data
 	 *
-	 * @return array
+	 * @return array<int,array<string,string>>
 	 */
 	public function get_calender_data()
 	{
 		// phpcs:enable
-		return $this->cal['VCALENDAR'];
+		return $this->cal['VCALENDAR'];  // false-positive @phpstan-ignore-line
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
 	/**
 	 * Return array with all data
 	 *
-	 * @return array
+	 * @return  array<string,array<string,array<string,int>>>|array<string,array<'VEVENT'|'VFREEBUSY'|'VTODO',array<int,array<string,string>>>>|array<string,array<string,int>>|array<string,array<string,string>>
 	 */
 	public function get_all_data()
 	{
