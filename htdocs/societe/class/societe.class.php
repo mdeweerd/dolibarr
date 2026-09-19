b582e19f0bc076213643e1aca3faa9e4c29bd080
MDW <mdeweerd@users.noreply.github.com>
2026-09-19 19:14:23 +0200

    Remove deprecated code_compta property from Societe class
    - Remove public $code_compta property which is mapped to $code_compta_client in deprecatedProperties
    - This fixes verification error when Societe objects are destroyed during tests

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -905,12 +905,7 @@ class Societe extends CommonObject
 	 */
 	public $bank_account;
 
-	/**
-	 * @deprecated
-	 * Accounting code for client
-	 * @var ?string
-	 */
-	public $code_compta;
+
 
 
 	const STATUS_CEASED = 0;
